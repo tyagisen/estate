bbf938a4853a36f42b08e739e883d1c89cee83a6
biratrai10 <biratrai1111@gmail.com>
2020-10-23 08:42:25 +0545

    Login pages created and linked with dashboard

--- estateapp/urls.py
+++ estateapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.HomeListView.as_view(), name='home'),
     path('detail/<int:pk>/', views.HomeDetailView.as_view(), name='detail'),
     path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
+    path('login/', views.Login.as_view(), name='login'),
 
 ]
--- estateapp/views.py
+++ estateapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,HttpResponse,  HttpResponse, redirect, get_object_or_404
 from .models import *
 from django.views.generic import ListView, DetailView, RedirectView,TemplateView
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 class HomeListView(ListView):
     model = Home
@@ -18,3 +19,24 @@ class Dashboard(TemplateView):
     context_object_name= 'dashboard'
     template_name = 'estateapp/dashboard.html'
 
+class Login(TemplateView):
+    # model = Home
+    context_object_name= 'login'
+    template_name = '../templates/login.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return render(request, self.template_name)
+
+    def post(self,request,*args,**kwargs):
+
+        u=request.POST.get('username')
+        p=request.POST.get('pass')
+        user=authenticate(username=u,password=p)
+        if user is not None:
+            login(request,user)
+            return redirect('dashboard')
+        else:
+            messages.add_message(request,messages.ERROR,"Login Credentials doesnt match")
+            return redirect('home')
